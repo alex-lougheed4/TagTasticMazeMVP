4d6ad61f10ef98da4622c7028ebeac4be151954a
alex-lougheed4 <62121996+alex-lougheed4@users.noreply.github.com>
2021-02-17 23:52:07 +0000

    small changes to GUI to attempt to fix buttons

---
+++ Assets/Scripts/MenuNetworkScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror{
+
+
+public class MenuNetworkScript : MonoBehaviour
+{
+
+    NetworkManager manager;
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+
+
+}
+}
