154282555370c05552036ac1e325ea7333b2a781
alex-lougheed4 <62121996+alex-lougheed4@users.noreply.github.com>
2021-02-19 03:07:18 +0000

    GUI implementation script modification, requires testing and implementing onto the Menu Buttons for respective functions

--- Assets/Scripts/MenuNetworkScript.cs
+++ Assets/Scripts/MenuNetworkScript.cs
@@ -14,6 +14,152 @@ public class MenuNetworkScript : MonoBehaviour
     {
         manager = GetComponent<NetworkManager>();
     }
+/**       void OnGUI()
+        {
+
+            if (!NetworkClient.isConnected && !NetworkServer.active)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
+
+            // client ready
+            if (NetworkClient.isConnected && !ClientScene.ready)
+            {
+                if (GUILayout.Button("Client Ready"))
+                {
+                    ClientScene.Ready(NetworkClient.connection);
+
+                    if (ClientScene.localPlayer == null)
+                    {
+                        ClientScene.AddPlayer(NetworkClient.connection);
+                    }
+                }
+            }
+
+            StopButtons();
+
+            GUILayout.EndArea();
+        }
+        **/
+
+        void startHostServerClient(){ //Call for host button
+            if(!NetworkClient.active){
+                if (Application.platform != RuntimePlatform.WebGLPlayer)
+                {
+                    manager.StartHost();
+                }
+            }
+        }
+
+        void joinClient(){ //call for join with ip button
+            if(!NetworkClient.active){
+                
+                manager.StartClient();
+                
+            }
+            manager.networkAddress = ""; //insert ip from textfield here
+        }
+
+        void startServerOnly(){ //call for starting server only
+            if(!NetworkClient.active){
+                if(Application.platform != RuntimePlatform.WebGLPlayer){
+                    manager.StartServer();
+                }
+            }
+        }
+
+/**
+        void StartButtons()
+        {
+            if (!NetworkClient.active)
+            {
+                // Server + Client
+                if (Application.platform != RuntimePlatform.WebGLPlayer)
+                {
+                    if (GUILayout.Button("Host (Server + Client)"))
+                    {
+                        manager.StartHost();
+                    }
+                    
+                }
+
+
+                // Client + IP
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Client"))
+                {
+                    manager.StartClient();
+                }
+                manager.networkAddress = GUILayout.TextField(manager.networkAddress);
+                GUILayout.EndHorizontal();
+
+                // Server Only
+                if (Application.platform == RuntimePlatform.WebGLPlayer)
+                {
+                    // cant be a server in webgl build
+                    GUILayout.Box("(  WebGL cannot be server  )");
+                }
+                else
+                {
+                    if (GUILayout.Button("Server Only")) manager.StartServer();
+                }
+            }
+            else
+            {
+                // Connecting
+                GUILayout.Label("Connecting to " + manager.networkAddress + "..");
+                if (GUILayout.Button("Cancel Connection Attempt"))
+                {
+                    manager.StopClient();
+                }
+            }
+        }
+
+        void StatusLabels()
+        {
+            // server / client status message
+            if (NetworkServer.active)
+            {
+                GUILayout.Label("Server: active. Transport: " + Transport.activeTransport);
+            }
+            if (NetworkClient.isConnected)
+            {
+                GUILayout.Label("Client: address=" + manager.networkAddress);
+            }
+        }
+
+        void StopButtons()
+        {
+            // stop host if host mode
+            if (NetworkServer.active && NetworkClient.isConnected)
+            {
+                if (GUILayout.Button("Stop Host"))
+                {
+                    manager.StopHost();
+                }
+            }
+            // stop client if client-only
+            else if (NetworkClient.isConnected)
+            {
+                if (GUILayout.Button("Stop Client"))
+                {
+                    manager.StopClient();
+                }
+            }
+            // stop server if server-only
+            else if (NetworkServer.active)
+            {
+                if (GUILayout.Button("Stop Server"))
+                {
+                    manager.StopServer();
+                }
+            }
+        }
+        **/
 
 
 }
